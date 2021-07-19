243e6bb5fd478ff59330f0b3a0379110c5eb51c5
directx5 <73019195+directx5@users.noreply.github.com>
2021-07-19 23:33:54 +0300

    OOP

--- captcha.py
+++ captcha.py
@@ -1,35 +1,36 @@
-from time import sleep
-
 from requests import post
 
 
-def solve(api_key: str):
-    balance = float(post('https://2captcha.com/res.php', {'key': api_key, 'action': 'getbalance'}).text)
-    if balance > 0:
-        body = {
-            'key': api_key,
-            'method': 'hcaptcha',
-            'sitekey': 'a010c060-9eb5-498c-a7b9-9204c881f9dc',
-            'pageurl': 'https://signup.tr.leagueoflegends.com/tr/signup/index',
-            'soft_id': 2622,
-        }
+class TwoCaptcha:
+    def __init__(self, api_key):
+        self.api_key = api_key
 
-        captcha_id = post('https://2captcha.com/in.php', body).text.split('|')[1]
+    def balance(self):
+        return float(post('https://2captcha.com/res.php', {'key': self.api_key, 'action': 'getbalance'}).text)
 
-        body = {
-            'key': api_key,
-            'method': 'hcaptcha',
-            'action': 'get',
-            'id': captcha_id,
-            'soft_id': 2622,
-        }
+    def solve(self):
+        if self.balance() > 0:
+            body = {
+                'key': self.api_key,
+                'method': 'hcaptcha',
+                'sitekey': 'a010c060-9eb5-498c-a7b9-9204c881f9dc',
+                'pageurl': 'https://signup.tr.leagueoflegends.com/tr/signup/index',
+                'soft_id': 2622,
+            }
+            captcha_id = post('https://2captcha.com/in.php', body).text.split('|')[1]
 
-        token = post('https://2captcha.com/res.php', body, timeout=5).text
+            body = {
+                'key': self.api_key,
+                'method': 'hcaptcha',
+                'action': 'get',
+                'id': captcha_id,
+                'soft_id': 2622,
+            }
+            token = post('https://2captcha.com/res.php', body).text
 
-        while token == 'CAPCHA_NOT_READY':
-            sleep(5)
-            token = post('https://2captcha.com/res.php', body, timeout=5).text
+            while token == 'CAPCHA_NOT_READY':
+                token = post('https://2captcha.com/res.php', body, timeout=1).text
 
-        return token if (r := token.split('|')[1]) is None else r
-    else:
-        raise ValueError(f'Balance ran out!')
+            return token if (r := token.split('|')[1]) is None else r
+        else:
+            raise ValueError(f'Balance ran out!')
--- main.py
+++ main.py
@@ -2,37 +2,43 @@ from json import dumps
 from string import ascii_letters, digits
 from random import choices
 
-from captcha import solve
+from captcha import TwoCaptcha
 
 from requests import post
 
-api_key = 'API_KEY'
-api_url = 'https://signup-api.leagueoflegends.com/v1/accounts'
 
-balance = float(post('https://2captcha.com/res.php', {'key': api_key, 'action': 'getbalance'}).text)
+class Creator:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.api_url = 'https://signup-api.leagueoflegends.com/v1/accounts'
+        self.captcha = TwoCaptcha(self.api_key)
 
-if balance <= 0:
-    raise ValueError(f'Not enough balance! Balance: {balance}. Balance must be greater than 0.')
+    def create(self):
+        def data(length: int):
+            return ''.join(choices(ascii_letters + digits, k=length))
+
+        if self.captcha.balance() <= 0:
+            raise ValueError(f'Not enough balance! Balance: {self.captcha.balance()}. Balance must be greater than 0.')
+
+        request_body = {
+            'username': (username := data(16)),
+            'password': (password := data(16)),
+            'confirm_password': password,
+            'date_of_birth': '2000-01-01',
+            'email': (email := f'{username}@direct.com'),
+            'tou_agree': True,
+            'newsletter': False,
+            'region': 'TR1',
+            'campaign': 'league_of_legends',
+            'locale': 'tr',
+            'token': f'hcaptcha {self.captcha.solve()}',
+        }
+        response = post(url=self.api_url, data=dumps(request_body), headers={'Content-Type': 'application/json'},
+                        timeout=10)
+
+        return response.json(), dumps({'username': username, 'password': password, 'email': email})
 
 
-def generate(length: int):
-    return ''.join(choices(ascii_letters + digits, k=length))
-
-
-request_body = {
-        'username': (username := generate(14)),
-        'password': (password := generate(14)),
-        'confirm_password': password,
-        'date_of_birth': '2000-01-01',
-        'email': (email := f'{username}@direct.com'),
-        'tou_agree': True,
-        'newsletter': False,
-        'region': 'TR1',
-        'campaign': 'league_of_legends',
-        'locale': 'tr',
-        'token': f'hcaptcha {solve(api_key)}',
-    }
-response = post(url=api_url, data=dumps(request_body), headers={'Content-Type': 'application/json'}, timeout=10)
-
-print(response.json())
-print(dumps({'username': username, 'password': password, 'email': email}))
+if __name__ == '__main__':
+    creator = Creator('API_KEY')
+    print(creator.create())
